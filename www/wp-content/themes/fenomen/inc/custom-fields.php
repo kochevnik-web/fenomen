e66abba0ecd7a9a362234072b5658fcd6bf2db72
kovalev.d <kacevnik@yandex.ru>
2020-01-16 23:38:21 +0300

    added new fenomen settings and update db

--- www/wp-content/themes/fenomen/inc/custom-fields.php
+++ www/wp-content/themes/fenomen/inc/custom-fields.php
@@ -1,4 +1,7 @@
 <?php
+
+    define( 'SETTING_SLUG', 'fenomen-settings' );
+
     add_action( 'acf/init', 'add_fenomen_settings' );
 
     function add_fenomen_settings() {
@@ -7,7 +10,7 @@
             acf_add_options_page( array(
                 'page_title' 	  => 'Настройки темы Fenomen',
                 'menu_title'	  => 'Настройки Fenomen',
-                'menu_slug' 	  => 'fenomen-settings',
+                'menu_slug' 	  => SETTING_SLUG,
                 'capability'	  => 'edit_posts',
                 'update_button'   => __( 'Сохранить', 'fenomen' ),
                 'updated_message' => __( "Настройки сохранены", 'fenomen' ),
@@ -34,13 +37,63 @@
                             'width' => '50',
                         ),
                     ),
+                    array(
+                        'key'    => 'field_fenomen_head_enquqe_script',
+                        'label'  => 'Блок вставки в head',
+                        'name'   => 'fenomen_head_enquqe_script',
+                        'type'   => 'repeater',
+                        'min'    => 0,
+                        'max'    => 0,
+                        'layout' => 'table',
+                        'sub_fields' => array(
+                            array(
+                                'key'      => 'field_fenomen_head_enquqe_script_name',
+                                'label'    => 'Название',
+                                'name'     => 'fenomen_head_enquqe_script_name',
+                                'type'     => 'text',
+                                'required' => 1,
+                            ),
+                            array(
+                                'key'      => 'field_fenomen_head_enquqe_script_area',
+                                'label'    => 'Элемент вставки',
+                                'name'     => 'fenomen_head_enquqe_script_area',
+                                'type'     => 'textarea',
+                                'rows'     => 12,
+                            ),
+                        ),
+                    ),
+                    array(
+                        'key'    => 'field_fenomen_footer_enquqe_script',
+                        'label'  => 'Блок вставки в footer',
+                        'name'   => 'fenomen_footer_enquqe_script',
+                        'type'   => 'repeater',
+                        'min'    => 0,
+                        'max'    => 0,
+                        'layout' => 'table',
+                        'sub_fields' => array(
+                            array(
+                                'key'      => 'field_fenomen_footer_enquqe_script_name',
+                                'label'    => 'Название',
+                                'name'     => 'fenomen_footer_enquqe_script_name',
+                                'type'     => 'text',
+                                'required' => 1,
+                            ),
+                            array(
+                                'key'      => 'field_fenomen_footer_enquqe_script_area',
+                                'label'    => 'Элемент вставки',
+                                'name'     => 'fenomen_footer_enquqe_script_area',
+                                'type'     => 'textarea',
+                                'rows'     => 12,
+                            ),
+                        ),
+                    ),
                 ),
                 'location' => array(
                     array(
                         array(
                             'param' => 'options_page',
                             'operator' => '==',
-                            'value' => 'fenomen-settings',
+                            'value' => SETTING_SLUG,
                         ),
                     ),
                 ),
@@ -51,6 +104,57 @@
                 'instruction_placement' => 'label',
                 'active' => true,
             ));
+        }
+    }
+
+    add_action( 'wp_head', 'fenomen_add_head_scripts' );
+    add_action( 'wp_footer', 'fenomen_add_head_scripts' );
+    function fenomen_add_head_scripts() {
+        $hook = doing_filter( 'wp_head' ) ? 'head' : 'footer';
+        $opt  = get_option( 'options_fenomen_' . $hook . '_enquqe_script' );
+        if ( isset( $opt ) && $opt > 0 ) {
+            for( $i = 0; $i < $opt; $i++ ) {
+                ?>
+                    <!-- <?= get_option( 'options_fenomen_' . $hook . '_enquqe_script_' . $i . '_fenomen_' . $hook . '_enquqe_script_name' ) ?> -->
+                        <?= get_option( 'options_fenomen_' . $hook . '_enquqe_script_' . $i . '_fenomen_' . $hook . '_enquqe_script_area' ) ?>
+                    <!-- end <?= get_option( 'options_fenomen_' . $hook . '_enquqe_script_' . $i . '_fenomen_' . $hook . '_enquqe_script_name' ) ?> -->
+                <?php
+            }
+        }
+    }
+
+    add_action( 'admin_print_styles-toplevel_page_' . SETTING_SLUG, 'fenomen_add_code_styles' );
+    function fenomen_add_code_styles() {
+        $settings = wp_enqueue_code_editor( array( 'type' => 'text/html' ) );
+
+        if ( $settings === false ) return;
+
+        $id = '';
+        $count = get_option( 'options_fenomen_head_enquqe_script' );
+
+        if ( $count > 0 ) {
+            for ( $i = 0; $i < $count; $i++ ) {
+                $id = 'acf-field_fenomen_head_enquqe_script-row-' . $i . '-field_fenomen_head_enquqe_script_area';
+
+                wp_add_inline_script(
+                    'code-editor',
+                    sprintf( 'jQuery(function(){wp.codeEditor.initialize("%s", %s );} )', $id, wp_json_encode( $settings ) )
+                );
+                
+            }
+        }
+
+        $count = get_option( 'options_fenomen_footer_enquqe_script' );
+        if ( $count > 0 ) {
+            for ( $i = 0; $i < $count; $i++ ) {
+                $id = 'acf-field_fenomen_footer_enquqe_script-row-' . $i . '-field_fenomen_footer_enquqe_script_area';
+
+                wp_add_inline_script(
+                    'code-editor',
+                    sprintf( 'jQuery(function(){wp.codeEditor.initialize("%s", %s );} )', $id, wp_json_encode( $settings ) )
+                );
+                
+            }
         }
     }
 
