b17752036439cc881ed3a65f632fd8b46c09aef2
Dmitriy Kovalev <kodimweb@gmail.com>
2020-01-22 00:25:33 +0300

    starting form styles

--- www/wp-content/themes/fenomen/front-page.php
+++ www/wp-content/themes/fenomen/front-page.php
@@ -49,6 +49,35 @@ get_header();
 		</section>
 		<?php } ?>
 
+		<section id="main_form" class="form">
+			<div class="container">
+				<div class="row">
+					<div class="col-12">
+						<h3 class="text-center">
+						Запишитесь на <span class="color-blue">бесплатное</span> пробное занятие прямо сейчас
+						</h3>
+						<div class="sub_title text-center col-md-10 offset-md-1 mb-5">
+						Введите свой номер телефона, наш специалист свяжется с Вами, расскажет о преимуществах школы, ответит на все вопросы и запишет вашего ребенка на бесплатное занятие в удобное для Вас время
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-3">
+						<input type="text" placeholder="Ваше имя">
+					</div>
+					<div class="col-md-3">
+						<input type="text" placeholder="Номер телефона">
+					</div>
+					<div class="col-md-3">
+						<input type="text" placeholder="Удобная дата занятия">
+					</div>
+					<div class="col-md-3">
+						<button class="btn btn-primary btn-yellow w-100">Записаться на занятие</button>
+					</div>
+				</div>
+			</div>
+		</section>
+
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
