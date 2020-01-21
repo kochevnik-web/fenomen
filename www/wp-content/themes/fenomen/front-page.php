a7eae7f31a9c56839f01bfe31df0ee5bd391f21d
Dmitriy Kovalev <kacevnik@yandex.ru>
2020-01-21 16:32:13 +0300

    finish intro block

--- www/wp-content/themes/fenomen/front-page.php
+++ www/wp-content/themes/fenomen/front-page.php
@@ -18,59 +18,36 @@ get_header();
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
 
+		<?php if ( (bool)get_post_meta( $post->ID, 'main_tittle', true ) ) { ?>
 		<section id="intro">
 			<div class="container">
 				<div class="intro_bg">
 					<div class="row">
 						<div class="col-md-8 mb-2 mb-md-5">
-							<h1 class="color-white"><?php  echo get_post_meta( $post->ID, 'main_tittle', true ); ?></h1>
+							<h1 class="color-white"><?php echo get_post_meta( $post->ID, 'main_tittle', true ); ?></h1>
 						</div>
 					</div>
 					<div class="row">
-						<div class="intro_small_title color-white col-12 mb-3 mt-3 mt-mf-5">Выберите подходящую программу для вашего ребенка:</div>
+						<div class="intro_small_title color-white col-12 mb-3 mt-3 mt-mf-5"><?php echo get_post_meta( $post->ID, 'intro_sub_title', true ); ?></div>
+						<?php for ( $i = 1; $i <=3; $i++ ) { ?>
 						<div class="col-md-4 mb-4 mb-md-0">
+							<?php if ( (bool)get_post_meta( $post->ID, 'intro_prog_name_' . $i, true ) ) { ?>
 							<div class="intro_item text-center">
-								<img src="<?= get_template_directory_uri() . '/img/rocket.png' ?>" alt="" class="intro_item_img mb-3">
+								<img src="<?= wp_get_attachment_image_url( get_post_meta( $post->ID, 'intro_prog_img_' . $i, true ) ); ?>" alt="" class="intro_item_img mb-3">
 								<div class="intro_item_content">
-									<div class="h4">Феномен Start</div>
-									<p class="mb-4">Программа для детей от 5 лет и тех,
-кто только начинает изучать
-шахматы. Данная программа вызовет
-интерес к игре у ребенка</p>
+									<div class="h4"><?php echo get_post_meta( $post->ID, 'intro_prog_name_' . $i, true ); ?></div>
+									<p class="mb-4"><?php echo get_post_meta( $post->ID, 'intro_prog_desc_' . $i, true ); ?></p>
 								</div>
-								<a href="#" class="btn btn-primary intro_item_btn">Подробнее о программе</a>
+								<a href="<?php echo get_post_meta( $post->ID, 'intro_prog_link_' . $i, true ); ?>" class="btn btn-primary intro_item_btn">Подробнее о программе</a>
 							</div>
+							<?php } ?>
 						</div>
-						<div class="col-md-4 mb-4 mb-md-0">
-							<div class="intro_item text-center">
-								<img src="<?= get_template_directory_uri() . '/img/rocket.png' ?>" alt="" class="intro_item_img mb-3">
-								<div class="intro_item_content">
-									<div class="h4">Феномен Start</div>
-									<p class="mb-4">Программа для детей от 5 лет и тех,
-кто только начинает изучать
-шахматы. Данная программа вызовет
-интерес к игре у ребенка</p>
-								</div>
-								<a href="#" class="btn btn-primary intro_item_btn">Подробнее о программе</a>
-							</div>
-						</div>
-						<div class="col-md-4 mb-4 mb-md-0">
-							<div class="intro_item text-center">
-								<img src="<?= get_template_directory_uri() . '/img/rocket.png' ?>" alt="" class="intro_item_img mb-3">
-								<div class="intro_item_content">
-									<div class="h4">Феномен Start</div>
-									<p class="mb-4">Программа для детей от 5 лет и тех,
-кто только начинает изучать
-шахматы. Данная программа вызовет
-интерес к игре у ребенка</p>
-								</div>
-								<a href="#" class="btn btn-primary intro_item_btn">Подробнее о программе</a>
-							</div>
-						</div>
+						<?php } ?>
 					</div>
 				</div>
 			</div>
 		</section>
+		<?php } ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
