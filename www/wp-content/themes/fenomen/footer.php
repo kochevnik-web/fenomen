d3b674569dd0399223e9cb1db5869f99c4bd0b4c
Dmitriy Kovalev <kacevnik@yandex.ru>
2020-01-31 17:43:01 +0300

    started working footer

--- www/wp-content/themes/fenomen/footer.php
+++ www/wp-content/themes/fenomen/footer.php
@@ -13,21 +13,40 @@
 
 	</div><!-- #content -->
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'fenomen' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'fenomen' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'fenomen' ), 'fenomen', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+	<footer class="color-white">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-2">
+					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="logo inline-block mr-lg-5" rel="home">
+						<img src="<?= get_template_directory_uri() . '/img/logo.svg' ?>" alt="<?php bloginfo( 'name' ); ?>">
+					</a>
+				</div>
+				<div class="col-md-5">
+					<ul class="footer_menu list-unstyled m-0 d-flex">
+						<li><a href="#">Главная</a></li>
+						<li><a href="#">Онлайн-школа</a></li>
+						<li><a href="#">Мероприятия</a></li>
+						<li><a href="#">Программы</a></li>
+						<li><a href="#">Вопросы и ответы</a></li>
+						<li><a href="#">Блог</a></li>
+					</ul>
+				</div>
+				<div class="col-md-5">
+					<div class="footer_item">
+						<a href="#" class="btn btn-primary">Открыть школу</a>
+					</div>
+					<div class="footer_item">
+						<a href="#">Политика конфиденциальности</a>
+					</div>
+					<div class="footer_item">
+						<a href="<?= get_option( 'options_fenomen_facebook' ); ?>"><i class="fab fa-facebook"></i></a>
+						<a href="<?= get_option( 'options_fenomen_instagram' ); ?>"><i class="fab fa-instagram"></i></a>
+						<a href="<?= get_option( 'options_fenomen_vk' ); ?>"><i class="fab fa-vk"></i></a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</footer><!-- #footer -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
