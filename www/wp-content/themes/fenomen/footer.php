151c561857685445ddeff2b0f95a6d4efbd95a9f
Dmitriy Kovalev <kodimweb@gmail.com>
2020-02-02 22:06:39 +0300

    finish styles for footer

--- www/wp-content/themes/fenomen/footer.php
+++ www/wp-content/themes/fenomen/footer.php
@@ -16,13 +16,13 @@
 	<footer class="color-white">
 		<div class="container">
 			<div class="row">
-				<div class="col-md-2">
+				<div class="col-md-3 col-xl-2 mb-3 mb-xl-0 text-center text-md-left">
 					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="logo inline-block mr-lg-5" rel="home">
 						<img src="<?= get_template_directory_uri() . '/img/logo.svg' ?>" alt="<?php bloginfo( 'name' ); ?>">
 					</a>
 				</div>
-				<div class="col-md-5">
-					<ul class="footer_menu list-unstyled m-0 d-flex">
+				<div class="col-md-9 col-xl-5">
+					<ul class="footer_menu list-unstyled m-0 d-flex text-center text-md-right text-xl-left">
 						<li><a href="#">Главная</a></li>
 						<li><a href="#">Онлайн-школа</a></li>
 						<li><a href="#">Мероприятия</a></li>
@@ -31,17 +31,17 @@
 						<li><a href="#">Блог</a></li>
 					</ul>
 				</div>
-				<div class="col-md-5">
-					<div class="footer_item">
-						<a href="#" class="btn btn-primary">Открыть школу</a>
+				<div class="col-xl-5 d-block d-md-flex align-items-center text-center">
+					<div class="footer_item mb-4 mb-md-0 text-center text-md-left">
+						<a href="http://dev.fenomen.loc/privacy-policy/" class="btn btn-primary btn-white-border">Открыть школу</a>
 					</div>
-					<div class="footer_item">
-						<a href="#">Политика конфиденциальности</a>
+					<div class="footer_item text-center text-xl-left">
+						<a href="#" class="politic">Политика конфиденциальности</a>
 					</div>
-					<div class="footer_item">
-						<a href="<?= get_option( 'options_fenomen_facebook' ); ?>"><i class="fab fa-facebook"></i></a>
-						<a href="<?= get_option( 'options_fenomen_instagram' ); ?>"><i class="fab fa-instagram"></i></a>
-						<a href="<?= get_option( 'options_fenomen_vk' ); ?>"><i class="fab fa-vk"></i></a>
+					<div class="footer_item text-center text-md-right">
+						<a href="<?= get_option( 'options_fenomen_facebook' ); ?>" class="contacts_link"><i class="fab fa-facebook"></i></a>
+						<a href="<?= get_option( 'options_fenomen_instagram' ); ?>" class="contacts_link"><i class="fab fa-instagram"></i></a>
+						<a href="<?= get_option( 'options_fenomen_vk' ); ?>" class="contacts_link"><i class="fab fa-vk"></i></a>
 					</div>
 				</div>
 			</div>
