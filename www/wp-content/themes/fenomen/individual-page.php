be7e6b07f81b472273e56c7bca80d1014c758cbb
Dmitriy Kovalev <kodimweb@gmail.com>
2020-02-02 22:45:38 +0300

    start to work with programm pages

---
+++ www/wp-content/themes/fenomen/individual-page.php
@@ -0,0 +1,18 @@
+<?php
+/*
+Template Name: Страница Individual
+Template Post Type: page
+*/
+
+    get_header();
+?>
+
+    <div id="primary" class="content-area">
+		<main id="main" class="site-main">
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+
+get_footer();
