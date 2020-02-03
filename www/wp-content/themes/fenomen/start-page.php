8505ee80efe40d148e3b2860b63adcc871c6b912
Dmitriy Kovalev <kodimweb@gmail.com>
2020-02-03 23:34:24 +0300

    finish book section on start page

--- www/wp-content/themes/fenomen/start-page.php
+++ www/wp-content/themes/fenomen/start-page.php
@@ -80,34 +80,36 @@ Template Post Type: page
             </section>
             <?php } ?>
 
+            <?php if ( (bool)get_post_meta( $post->ID, 'book_title', true ) ) { ?>
             <section id="book" class="white_bg position-relative">
                 <div class="container">
-                    <h3 class="text-center mb-0 mb-md-5">Познакомьтесь с учебными<br>материалами школы Феномен</h3>
+                    <h3 class="text-center mb-0 mb-md-5"><?= get_post_meta( $post->ID, 'book_title', true ); ?></h3>
                     <div class="row">
                         <div class="col-12">
                             <div class="book_elem position-relative d-flex d-xl-block mt-5 mt-xl-0">
                                 <div id="book_elem_1" class="mb-4 mb-md-5 mb-lg-0">
                                     <div class="number">01</div>
-                                    <h3 class="color-blue">Видеоуроки домашнего задания</h3>
+                                    <h3 class="color-blue"><?= get_post_meta( $post->ID, 'book_slogan_1', true ); ?></h3>
                                 </div>
                                 <div id="book_elem_2" class="mb-4 mb-md-5 mb-lg-0">
                                     <div class="number">02</div>
-                                    <h3 class="color-blue">Творческие задания</h3>
+                                    <h3 class="color-blue"><?= get_post_meta( $post->ID, 'book_slogan_2', true ); ?></h3>
                                 </div>
                                 <div id="book_elem_3" class="mb-4 mb-md-5 mb-lg-0">
                                     <div class="number">03</div>
-                                    <h3 class="color-blue">Интересные задачи</h3>
+                                    <h3 class="color-blue"><?= get_post_meta( $post->ID, 'book_slogan_3', true ); ?></h3>
                                     <div>Более 200 уроков</div>
                                 </div>
                                 <div id="book_elem_4" class="mb-4 mb-md-5 mb-lg-0">
                                     <div class="number">04</div>
-                                    <h3 class="color-blue">Современный дизайн</h3>
+                                    <h3 class="color-blue"><?= get_post_meta( $post->ID, 'book_slogan_4', true ); ?></h3>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </section>
+            <?php } ?>
 
             <section id="contacts_section" class="color-white position-relative">
                 <div class="container">
