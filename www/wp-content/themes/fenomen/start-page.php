a764c4b8c8031b2087f66b4b02e27e688a124c4c
Dmitriy Kovalev <kacevnik@yandex.ru>
2020-02-03 10:59:28 +0300

    finish front for start_list section

--- www/wp-content/themes/fenomen/start-page.php
+++ www/wp-content/themes/fenomen/start-page.php
@@ -47,6 +47,29 @@ Template Post Type: page
             </section>
             <?php } ?>
 
+            <section id="start_list" class="white_bg">
+                <div class="container">
+                    <h3 class="text-center mb-5">Чему научится Ваш ребенок?</h3>
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="start_list_item">
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <img src="<?= get_template_directory_uri() . '/img/start_list_1.png' ?>" alt="" class="w-100 pr-lg-4">
+                                    </div>
+                                    <div class="col-md-6 mb-3 mb-md-0">
+                                        <div class="pl-lg-4">
+                                            <h4 class="color-blue mb-3 mb-lg-5">Сформирует логическое и аналитическое мышление</h4>
+                                            <p>Способность учиться складывается прежде всего из любознательности, жажды исследований, «прокачанности»                  критического мышления. Имеется в виду умение работать с информацией: анализировать, сомневаться в недоказанном,         сравнивать разные точки зрения, делать выводы, «глубоко копать», выбирать оптимальный способ решения.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div><!-- #start_list_item -->
+                        </div>
+                    </div>
+                </div>
+            </section>
+
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
