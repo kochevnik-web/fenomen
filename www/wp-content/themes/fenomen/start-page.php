110479bdec49ffcecb291f4a7a9298ddd4cb5292
Dmitriy Kovalev <kacevnik@yandex.ru>
2020-02-03 11:51:40 +0300

    finish front and back for start_list section

--- www/wp-content/themes/fenomen/start-page.php
+++ www/wp-content/themes/fenomen/start-page.php
@@ -47,28 +47,38 @@ Template Post Type: page
             </section>
             <?php } ?>
 
+            <?php if ( (bool)get_post_meta( $post->ID, 'start_list_title', true ) ) { ?>
             <section id="start_list" class="white_bg">
                 <div class="container">
-                    <h3 class="text-center mb-5">Чему научится Ваш ребенок?</h3>
+                    <h3 class="text-center mb-5"><?= get_post_meta( $post->ID, 'start_list_title', true ); ?></h3>
                     <div class="row">
                         <div class="col-12">
+                            <?php
+                                if ( get_post_meta( $post->ID, 'start_list', true ) > 0 ) {
+                                    for ( $i = 0; $i < get_post_meta( $post->ID, 'start_list', true ); $i++ ) {
+                                        $start_list_class_1 = $i%2 ? ' flex-row-reverse' : '';
+                                        $start_list_class_2 = $i%2 ? 'pl-lg-4' : 'pr-lg-4';
+                                        $start_list_class_3 = $i%2 ? 'pr-lg-4' : 'pl-lg-4';
+                            ?>
                             <div class="start_list_item">
-                                <div class="row">
-                                    <div class="col-md-6">
-                                        <img src="<?= get_template_directory_uri() . '/img/start_list_1.png' ?>" alt="" class="w-100 pr-lg-4">
+                                <div class="row<?= $start_list_class_1; ?>">
+                                    <div class="col-md-6 mb-3 mb-md-0 <?= $start_list_class_2; ?>">
+                                        <img src="<?= wp_get_attachment_image_url( get_post_meta( $post->ID, 'start_list_' . $i . '_img', true ), 'full' ); ?>" alt="<?= get_post_meta( $post->ID, 'start_list_' . $i . '_name', true ); ?>" class="w-100">
                                     </div>
-                                    <div class="col-md-6 mb-3 mb-md-0">
-                                        <div class="pl-lg-4">
-                                            <h4 class="color-blue mb-3 mb-lg-5">Сформирует логическое и аналитическое мышление</h4>
-                                            <p>Способность учиться складывается прежде всего из любознательности, жажды исследований, «прокачанности»                  критического мышления. Имеется в виду умение работать с информацией: анализировать, сомневаться в недоказанном,         сравнивать разные точки зрения, делать выводы, «глубоко копать», выбирать оптимальный способ решения.</p>
+                                    <div class="col-md-6">
+                                        <div class="<?= $start_list_class_3; ?>">
+                                            <h4 class="color-blue mb-3 mb-lg-5"><?= get_post_meta( $post->ID, 'start_list_' . $i . '_name', true ); ?></h4>
+                                            <p><?= get_post_meta( $post->ID, 'start_list_' . $i . '_text', true ); ?></p>
                                         </div>
                                     </div>
                                 </div>
                             </div><!-- #start_list_item -->
+                            <?php } } ?>
                         </div>
                     </div>
                 </div>
             </section>
+            <?php } ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
