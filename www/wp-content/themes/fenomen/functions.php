bd7ae6786e8fafa1657a6a7e828196e1e7762a46
Dmitriy Kovalev <kodimweb@gmail.com>
2020-02-15 15:00:29 +0300

    added single-post page

--- www/wp-content/themes/fenomen/functions.php
+++ www/wp-content/themes/fenomen/functions.php
@@ -229,3 +229,35 @@ add_filter( 'get_the_archive_title', function( $title ) {
 	return str_replace( 'Рубрика: ', '', $title );
 });
 
+// add_action( 'init', 'add_post_type' );
+// function my_custom_init(){
+// 	register_post_type('book', array(
+// 		'labels'             => array(
+// 			'name'               => 'Книги', // Основное название типа записи
+// 			'singular_name'      => 'Книга', // отдельное название записи типа Book
+// 			'add_new'            => 'Добавить новую',
+// 			'add_new_item'       => 'Добавить новую книгу',
+// 			'edit_item'          => 'Редактировать книгу',
+// 			'new_item'           => 'Новая книга',
+// 			'view_item'          => 'Посмотреть книгу',
+// 			'search_items'       => 'Найти книгу',
+// 			'not_found'          =>  'Книг не найдено',
+// 			'not_found_in_trash' => 'В корзине книг не найдено',
+// 			'parent_item_colon'  => '',
+// 			'menu_name'          => 'Книги'
+
+// 		  ),
+// 		'public'             => true,
+// 		'publicly_queryable' => true,
+// 		'show_ui'            => true,
+// 		'show_in_menu'       => true,
+// 		'query_var'          => true,
+// 		'rewrite'            => true,
+// 		'capability_type'    => 'post',
+// 		'has_archive'        => true,
+// 		'hierarchical'       => false,
+// 		'menu_position'      => null,
+// 		'supports'           => array('title','editor','author','thumbnail','excerpt','comments')
+// 	) );
+// }
+
--- www/wp-content/themes/fenomen/single.php
+++ www/wp-content/themes/fenomen/single.php
@@ -1,37 +1,39 @@
 <?php
-/**
- * The template for displaying all single posts
+/*
  *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  *
- * @package fenomen
+ * @package fenomen category page
  */
 
-get_header();
+    $paged = get_query_var('paged') ?: 1;
+
+    get_header();
 ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
+            <div class="container">    
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
-
-			get_template_part( 'template-parts/content', get_post_type() );
-
-			the_post_navigation();
-
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
-		endwhile; // End of the loop.
-		?>
-
+                <div class="row mb-4">
+					<div class="col-12">
+						<?php
+							while ( have_posts() ) :
+								the_post();
+								?>
+								<h1 class="font-weight-bold mb-4"><?php the_title(); ?></h1>
+								<div id="mb-4">
+									<?php the_content(); ?>
+								</div>
+								<?php
+							endwhile; // End of the loop.
+						?>
+					</div>
+				</div>
+            </div>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
 <?php
-get_sidebar();
+
 get_footer();
